072ffe1a8ad904027904d8a5f4522247bbddd122
Dhanielgeek <dhanielknightz46@gmail.com>
2024-09-02 19:08:55 +0100

    testing..

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./App.tsx";
+import App from "./App.tsx";
 import "./index.css";
 import "./Styles/custom.scss";
 import { Toaster } from "react-hot-toast";
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <Toaster />
-      {/* <App /> */}
+      <App />
     </Provider>
   </React.StrictMode>
 );
